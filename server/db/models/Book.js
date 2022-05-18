d5cb4020e61d9273ce744d827bf2c6884a3bdaf5
Shinichi <miyakawashi@gmail.com>
2022-05-17 22:01:19 -0400

    book model only ID

---
+++ server/db/models/Book.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../connection");
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "book",
+  }
+);
+
+module.exports = Book;
