3327f2e4e773aa08f00b1718472b712853d53a6e
Shinichi <miyakawashi@gmail.com>
2022-07-24 23:26:02 -0400

    singular book reroute

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 export default class Book extends React.Component {
   redirectToBook = () => {
     console.log("test");
-    let navigate = useNavigate();
     const bookId = this.props.id;
-    navigate(`/book/${bookId}`);
+    window.location.pathname = `/book/${bookId}`;
   };
-  // handleClick = (e) => {
-  //   this.redirectToBook();
-  // };
+
   render() {
     return (
       <div className="card" id={this.props.id} onClick={this.redirectToBook}>
