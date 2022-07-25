acb2da557f3c1afff551ab78ccbad99d1e7a9c3d
Shinichi <miyakawashi@gmail.com>
2022-07-24 22:33:46 -0400

    simple cards for book page

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Card extends React.Component {
   render() {
     return (
-      <div key={this.props.id}>
+      <div className="bookCard " key={this.props.id}>
         <div>Title: {this.props.title}</div>
         <div>Author: {this.props.author}</div>
       </div>
